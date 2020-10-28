d3260e63ec2b528de0d1b3c8738f7eb04fe2e929
Kyle Pelton <peltonykyle@gmail.com>
2020-10-28 00:12:17 -0700

    vga: Implement scrolling
    Signed-off-by: Kyle Pelton <peltonykyle@gmail.com>

--- vga.c
+++ vga.c
@@ -4,7 +4,7 @@ static int cursor_x = 0;
 static int cursor_y = 0;
 
 static unsigned char *vram = (unsigned char *)0xffffffff800B8000;
-
+//to be moved to stack once it is adjusted
 void update_cursor(int row, int col) {
     unsigned short position=(row*80) + col;
     // cursor LOW port to vga INDEX register
@@ -17,12 +17,35 @@ void update_cursor(int row, int col) {
 
 void vga_clear(){
     int i;
-
+    asm("cli");
     for(i=0; i<(80*25)*2; i++) {
         vram[i] = 0x00;
     }
+    asm("cli");
 }
-
+void vga_scroll(){
+    int i=0;
+    int j=0;
+    int offset = 0;
+    int from_offset = 0;
+    asm("cli");
+        for(j=1; j<25; j+=1) {
+            for(i=0; i<80;  i+=1) {
+                offset = (i*2)+(((j-1)*80)*2);
+                from_offset = (i*2)+(((j)*80)*2);
+                vram[offset] = vram[from_offset];
+                vram[offset+1] = vram[from_offset+1];
+                //vram[offset+1] = vram[from_offset+1] ;
+        }
+    }
+    for(i=0; i<80;  i+=1) {
+        offset = (i*2)+(((24)*80)*2);
+        vram[offset]=0;
+        vram[offset+1]=0;
+    }
+ 
+    asm("sti");
+}
 
 void print_loc(const int x, const int y, 
         const int back, const int fore, 
@@ -35,12 +58,18 @@ void print_loc(const int x, const int y,
 
 
 void kprintf(char *str) {
-
     for(;*str !='\0'; str++) {
 
         if (*str == '\n') {
             cursor_y++;
             cursor_x=0; 
+            if (cursor_y >=25){
+                vga_scroll();
+
+                cursor_y=24;
+            }
+
+
         } else {
             print_loc(cursor_x,cursor_y,0, 3,*str,0);
             cursor_x++;   
@@ -48,16 +77,15 @@ void kprintf(char *str) {
         if(cursor_x >= 80){
             cursor_y++;
             cursor_x=0;
-        }
-
-        if (cursor_y >= 25){
-            cursor_y = 4;
-            cursor_x = 0;
+             if (cursor_y ==25){
+                vga_scroll();
+                cursor_y=24;
+            }
         }
         update_cursor(cursor_y,cursor_x);
     }
 }
-char * itoa( unsigned int value, char * str, int base )
+char * itoa( unsigned long value, char * str, int base )
 {
     char * rc;
     char * ptr;
@@ -70,11 +98,7 @@ char * itoa( unsigned int value, char * str, int base )
     }
     rc = ptr = str;
     // Set '-' for negative decimals.
-    if (  base == 10 )
-    {
-        *ptr++ = '-';
-    }
-    // Remember where the numbers start.
+   // Remember where the numbers start.
     low = ptr;
     // The actual conversion.
     do
@@ -94,10 +118,15 @@ char * itoa( unsigned int value, char * str, int base )
     }
     return rc;
 }
-void print_regs() {
+void print_regs(unsigned long exception) {
     struct RegDump dump;
     char buffer[50];
-    //kprintf("\nKERNEL PANIC\nREGISTER DUMP\n=============\n");
+    kprintf("\nKERNEL PANIC\nREGISTER DUMP\n=============\n");
+    kprintf("EXCEPTION:");
+    itoa(exception,buffer,16);
+    kprintf(buffer);
+    kprintf("\n");
+    itoa(dump.rax,buffer,16);
     dump = dump_regs();
     itoa(dump.rax,buffer,16);
     kprintf("rax:0x");
--- vga.h
+++ vga.h
@@ -2,7 +2,7 @@
 #define VGA_H
 void kprintf(char *str);
 void vga_clear(void);
-char * itoa( unsigned int value, char * str, int base );
+char * itoa( unsigned long value, char * str, int base );
 void print_loc(const int x, const int y, const int back, const int fore, const char c, const int blink);
 void pring_regs();
 #endif
