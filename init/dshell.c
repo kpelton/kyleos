7535ad9157d9f5ae93afb1c4a630553231d723de
Kyle Pelton <peltonykyle@gmail.com>
2021-06-10 16:58:23 -0700

    dshell: print fake exception
    Signed-off-by: Kyle Pelton <peltonykyle@gmail.com>

--- init/dshell.c
+++ init/dshell.c
@@ -222,7 +222,7 @@ for(;;) {
         else if (kstrcmp(buffer,"mem\n") == 0) {
             mm_print_stats();
         }else if (kstrcmp(buffer,"panic\n") == 0) {
-            print_regs(0,0);
+            print_regs(0xdeadbeef,0xdeadbeef);
         }else if (kstrcmp(buffer,"jiffies\n") == 0) {
             kprint_hex("Jiffies 0x",read_jiffies());
         }else if (kstrcmp(buffer,"gas\n") == 0) {
