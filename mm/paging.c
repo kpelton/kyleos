d9ba1501dd4ada96ffe358f7b2433d6126ffd984
Kyle Pelton <peltonykyle@gmail.com>
2021-12-29 14:27:20 -0800

    mm/paging.c: Remove hard coding
    Signed-off-by: Kyle Pelton <peltonykyle@gmail.com>

--- mm/paging.c
+++ mm/paging.c
@@ -10,7 +10,13 @@
 #define VIRT_TO_PAGE_DIR(x) ((0x3fe00000 & x) >> 21) & 0x1ff
 #define VIRT_TO_PAGE_TAB(x) (((0x1ff000 & x) >> 12) & 0x1ff)
 #define VIRT_TO_PTE(x) ((0xfff & x) & 0xfff)
+#define PHYS_ADDR_MASK 0xffffffffff000
 
+#define PAGE_AVAIL 1
+#define READ_WRITE 1<<2
+#define SUPERVISOR 1<<3
+#define KERNEL_PAGE PAGE_AVAIL | READ_WRITE
+#define USER_PAGE PAGE_AVAIL | READ_WRITE | SUPERVISOR
 
 uint64_t initial_pml4[512] __attribute__((aligned(0x20)));         // must be aligned to (at least)0x20, ...
 uint64_t initial_page_dir_tab[512] __attribute__((aligned(0x20))); // must be aligned to (at least)0x20, ...
@@ -45,17 +51,17 @@ void early_setup_paging()
     uint64_t j = 0;
     uint64_t address = 0;
 
-    initial_pml4[511] = ((uint64_t)&initial_page_dir_tab - addr_start) | 7;     // set the page directory into the PDPT and mark it present
-    initial_page_dir_tab[510] = ((uint64_t)&initial_page_dir - addr_start) | 7; // set the page directory into the PDPT and mark it present
+    initial_pml4[511] = ((uint64_t)&initial_page_dir_tab - addr_start) | KERNEL_PAGE;     // set the page directory into the PDPT and mark it present
+    initial_page_dir_tab[510] = ((uint64_t)&initial_page_dir - addr_start) | KERNEL_PAGE; // set the page directory into the PDPT and mark it present
 
     j = 0;
     for (i = 0; i < INITIAL_PAGE_TAB; i++)
     {
-        initial_page_dir[i] = ((uint64_t)&initial_page_tab[j] - addr_start) | 7; //set the page table into the PD and mark it present/writable
+        initial_page_dir[i] = ((uint64_t)&initial_page_tab[j] - addr_start) | KERNEL_PAGE; //set the page table into the PD and mark it present/writable
         early_fill_dir((0x200000 * j), initial_page_tab[j]);
         j += 1;
     }
-    initial_pml4[273] = KERN_VIRT_TO_PHYS(&initial_iden_page_dir_tab) | 3;
+    initial_pml4[273] = KERN_VIRT_TO_PHYS(&initial_iden_page_dir_tab) | KERNEL_PAGE;
     address = 0;
     for(i=0; i<512; i++) {
         initial_iden_page_dir_tab[i] = address | 131;
@@ -86,30 +92,30 @@ bool paging_map_kernel_range(uint64_t start, uint64_t len)
 
         //kprintf("1 pkernel_pml4:%x %d\n",pkernel_pml4,offset);
         if ((pkernel_pml4[offset] & 1) == 0) {
-            pkernel_pml4[offset] = (uint64_t)pmem_alloc_zero_page() | 3;
+            pkernel_pml4[offset] = (uint64_t)pmem_alloc_zero_page() | KERNEL_PAGE;
         }
-        curr = (uint64_t *)KERN_PHYS_TO_PVIRT((pkernel_pml4[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *)KERN_PHYS_TO_PVIRT((pkernel_pml4[offset] & PHYS_ADDR_MASK));
         offset = VIRT_TO_PAGE_DIR_TAB_ADDR(virt_curr_addr);
 
         //kprintf("2 curr:%x %d\n",curr,offset);
         //kprintf("0x%x\n",curr);
         if ((curr[offset] & 1) == 0) {
-            curr[offset] = (uint64_t)pmem_alloc_zero_page() |3 ;
+            curr[offset] = (uint64_t)pmem_alloc_zero_page() | KERNEL_PAGE ;
         }
-        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & PHYS_ADDR_MASK));
 
         //kprintf("0x%x\n",curr);
         offset = VIRT_TO_PAGE_DIR(virt_curr_addr);
 
         //kprintf("3 curr:%x %d\n",curr,offset);
         if ((curr[offset] & 1) == 0) {
-            curr[offset] = (uint64_t)pmem_alloc_zero_page() |3 ;
+            curr[offset] = (uint64_t)pmem_alloc_zero_page() | KERNEL_PAGE ;
         }
-        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & PHYS_ADDR_MASK));
         offset = VIRT_TO_PAGE_TAB(virt_curr_addr);
         //kprintf("4 curr:%x %d\n",curr,offset);
         //kprintf("Writing to %x\n",curr+offset);
-        curr[offset] = phys_curr_addr | 3;
+        curr[offset] = phys_curr_addr | KERNEL_PAGE;
         phys_curr_addr += 0x1000;
         virt_curr_addr += 0x1000;
     }
@@ -134,10 +140,10 @@ bool paging_map_user_range(struct pg_tbl *pg, uint64_t start, uint64_t virt_star
         if ((curr[offset] & 1) == 0) {
                //     kprintf("Writing to %x\n",curr+offset);
 
-            curr[offset] = (uint64_t)pmem_alloc_zero_page()| 7;
+            curr[offset] = (uint64_t)pmem_alloc_zero_page()| USER_PAGE;
             //kprintf("new pagedirtab %x %x\n",curr[offset],offset);
         }
-        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & PHYS_ADDR_MASK));
         offset = VIRT_TO_PAGE_DIR_TAB_ADDR(virt_curr_addr);
        // kprintf("page_tab %x %x %x\n",curr,curr[offset],offset);
 
@@ -145,24 +151,24 @@ bool paging_map_user_range(struct pg_tbl *pg, uint64_t start, uint64_t virt_star
         if ((curr[offset] & 1) == 0) {
                     //kprintf("Writing to %x\n",curr+offset);
 
-            curr[offset] = (uint64_t)pmem_alloc_zero_page() |7 ;
+            curr[offset] = (uint64_t)pmem_alloc_zero_page() | USER_PAGE ;
         }
-        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & PHYS_ADDR_MASK));
         offset = VIRT_TO_PAGE_DIR(virt_curr_addr);
        /// kprintf("page_dir %x %x %x\n",curr,curr[offset],offset);
         //        kprintf("done1\n");
         if ((curr[offset] & 1) == 0) {
        //                     kprintf("done2\n");
       //  kprintf("Writing to %x\n",curr+offset);
-            curr[offset] = (uint64_t)pmem_alloc_zero_page() |7 ;
+            curr[offset] = (uint64_t)pmem_alloc_zero_page() | USER_PAGE ;
         }
            //     kprintf("done\n");
 
-        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & 0xffffffffffffff00));
+        curr = (uint64_t *) KERN_PHYS_TO_PVIRT((curr[offset] & PHYS_ADDR_MASK));
         offset = VIRT_TO_PAGE_TAB(virt_curr_addr);
       //  kprintf("page\n");
       //  kprintf("Writing to %x\n",curr+offset);
-        curr[offset] = phys_curr_addr | 7;
+        curr[offset] = phys_curr_addr | USER_PAGE;
 
         phys_curr_addr += 0x1000;
         virt_curr_addr += 0x1000;
@@ -176,8 +182,8 @@ static void paging_free_page_dir(uint64_t* pgdir_addr) {
 
     for (i=0; i<512; i++) {
         if((pgdir_addr[i]  &1) != 0) {
-            kprintf("free pgdir %x\n",pgdir_addr[i] & 0xffffffffffffff00 );
-            pmem_free_block(pgdir_addr[i] & 0xffffffffffffff00);
+            kprintf("free pgdir %x\n",pgdir_addr[i] & PHYS_ADDR_MASK );
+            pmem_free_block(pgdir_addr[i] & PHYS_ADDR_MASK);
         } 
     }
 }
@@ -187,9 +193,9 @@ static void paging_free_page_tab(uint64_t* pgtb_addr) {
 
     for (i=0; i<512; i++) {
         if((pgtb_addr[i]  &1) != 0) {
-            paging_free_page_dir((uint64_t*)KERN_PHYS_TO_PVIRT((pgtb_addr[i] & 0xffffffffffffff00)));
-            kprintf("free pgtab %x\n",pgtb_addr[i] & 0xffffffffffffff00 );
-            pmem_free_block(pgtb_addr[i] & 0xffffffffffffff00);
+            paging_free_page_dir((uint64_t*)KERN_PHYS_TO_PVIRT((pgtb_addr[i] & PHYS_ADDR_MASK)));
+            kprintf("free pgtab %x\n",pgtb_addr[i] & PHYS_ADDR_MASK );
+            pmem_free_block(pgtb_addr[i] & PHYS_ADDR_MASK);
         } 
     }
 }
@@ -199,13 +205,13 @@ static void paging_free_pml4(uint64_t* pml4_addr) {
 
     for (i=0; i<512; i++) {
         if((pml4_addr[i]  &1) != 0) {
-            paging_free_page_tab((uint64_t*) KERN_PHYS_TO_PVIRT((pml4_addr[i] & 0xffffffffffffff00)));
-            kprintf("free pml4 %x\n",pml4_addr[i] & 0xffffffffffffff00 );
-            pmem_free_block(pml4_addr[i] & 0xffffffffffffff00);
+            paging_free_page_tab((uint64_t*) KERN_PHYS_TO_PVIRT((pml4_addr[i] & PHYS_ADDR_MASK)));
+            kprintf("free pml4 %x\n",pml4_addr[i] & PHYS_ADDR_MASK );
+            pmem_free_block(pml4_addr[i] & PHYS_ADDR_MASK);
         } 
 
     }
-    pmem_free_block((KERN_PVIRT_TO_PHYS(pml4_addr) & 0xffffffffffffff00));
+    pmem_free_block((KERN_PVIRT_TO_PHYS(pml4_addr) & PHYS_ADDR_MASK));
 }
 
 bool paging_free_pg_tbl(struct pg_tbl *pg)
@@ -268,8 +274,8 @@ bool setup_paging()
     kernel_page_dir = (uint64_t *)KERN_PHYS_TO_VIRT(pmem_alloc_zero_page());
     kernel_iden_page_dir_tab = (uint64_t *)KERN_PHYS_TO_VIRT(pmem_alloc_zero_page());
 
-    kernel_pml4[VIRT_TO_PML4_ADDR(virt_curr_addr)] = KERN_VIRT_TO_PHYS(kernel_page_dir_tab) | 3;
-    kernel_pml4[273] = KERN_VIRT_TO_PHYS(kernel_iden_page_dir_tab) | 3;
+    kernel_pml4[VIRT_TO_PML4_ADDR(virt_curr_addr)] = KERN_VIRT_TO_PHYS(kernel_page_dir_tab) | KERNEL_PAGE;
+    kernel_pml4[273] = KERN_VIRT_TO_PHYS(kernel_iden_page_dir_tab) | KERNEL_PAGE;
     address = 0;
     kprintf("%x\n",kernel_iden_page_dir_tab);
     for(i=0; i<512; i++) {
@@ -277,7 +283,7 @@ bool setup_paging()
         address+=0x40000000;
     }
 
-    kernel_page_dir_tab[VIRT_TO_PAGE_DIR_TAB_ADDR(virt_curr_addr)] = KERN_VIRT_TO_PHYS(kernel_page_dir) | 3;
+    kernel_page_dir_tab[VIRT_TO_PAGE_DIR_TAB_ADDR(virt_curr_addr)] = KERN_VIRT_TO_PHYS(kernel_page_dir) | KERNEL_PAGE;
     page_tbl = (uint64_t **)KERN_PHYS_TO_VIRT(pmem_alloc_zero_page());
     for (i = 0; i != 511; i++)
     {
@@ -290,8 +296,8 @@ bool setup_paging()
     while (curr_addr <= address)
     {
         // kprintf("%x\n",curr_addr);
-        kernel_page_dir[i] = KERN_VIRT_TO_PHYS(page_tbl[i]) | 3;
-        page_tbl[i][j] = curr_addr | 3;
+        kernel_page_dir[i] = KERN_VIRT_TO_PHYS(page_tbl[i]) | KERNEL_PAGE;
+        page_tbl[i][j] = curr_addr | KERNEL_PAGE;
         curr_addr += 0x1000;
         if (curr_addr % 0x200000 == 0)
         {
